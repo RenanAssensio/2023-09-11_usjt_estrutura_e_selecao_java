aee78f20586d08d73a5454b682065202f0b470ef
Renan Assensio <renanassensio05@gmail.com>
2023-09-11 21:08:22 -0300

    ap03(selecao): fim do exemplo if

--- ExemploIf.java
+++ ExemploIf.java
@@ -3,12 +3,14 @@ import javax.swing.JOptionPane;
 public class ExemploIf{
     static public void main(String [] args){
       //declaração de variáveis
-      
-      //entrada
 
+      //entrada
+      double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
       //processamento
 
       //saída
-    }
-    
+      if(nota >= 70){
+        JOptionPane.showMessageDialog(null, "Aprovado");
+      }
+    } 
 }
